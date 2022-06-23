26d89287b2b86b1928f6bd924128802ef114ee0c
kholik03 <anurkholik03@gmail.com>
2022-06-23 11:01:24 +0700

    alpha

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\Products;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+public function getSearch(Request $request)
+    {
+      if ($request->has('search')){
+        $data['product'] = Products::where('product_name', 'LIKE', '%'.$request->search.'%');
+      } else {
+       return view('front.product_detail');
+      }
+    }
+
+    public static function getDetail($id)
+    {
+        $data['product'] = Products::findById($id);
+
+        return view('front.product_detail',$data);
+    }
+
+    
+}
---
+++ resources/views/backend/layout/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Maslik dibuat dengan <span class="heart"></span></span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
